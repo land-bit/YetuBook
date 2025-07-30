c18d989e6e8345580b525323e98b8e043d055055
land-bit <landrybitege7@gmail.com>
2025-07-30 16:28:41 +0200

    Finalisation de l'arrangement de la partie invitation d'amis sur homepage

--- Client/src/components/friendReqe/FriendReqe.jsx
+++ Client/src/components/friendReqe/FriendReqe.jsx
@@ -1,37 +1,35 @@
-import './friendReqe.css';
-import {Link} from "react-router-dom"
+import "./friendReqe.css";
+import { Link } from "react-router-dom";
 
 // FackApis...................
-import FriendReqData from "../../FackApis/FirendReqData"
+import FriendReqData from "../../FackApis/FirendReqData";
 
-export default function FriendReqe (){
-    return (
-        <div className='Friend-Request'>
-            <br />
-            <h4>Aliko la urafiki</h4>
+export default function FriendReqe() {
+  return (
+    <div className="Friend-Request">
+      <br />
+      <h4>{"Invitation d'amis"}</h4>
 
-            {
-                FriendReqData.map((friend, index)=>(
-                    <div className="request" key={index}>
-                        <Link to='/profile/id'>
-                            <div className="inform" >
-                                <div className="user">
-                                    <img src={friend.img} alt='' />
-                                    <h5>{friend.name}</h5>
-                                </div>
-                                <div className='info-name'>                                    
-                                    <p>{friend.info}</p>
-                                </div>
-                            </div>
-                        </Link>
+      {FriendReqData.map((friend, index) => (
+        <div className="request" key={index}>
+          <Link to="/profile/id">
+            <div className="inform">
+              <div className="user">
+                <img src={friend.img} alt="" />
+                <h5 className="text-foreground">{friend.name}</h5>
+              </div>
+              <div className="info-name">
+                <p>{friend.info} Amis en commun</p>
+              </div>
+            </div>
+          </Link>
 
-                        <div className="action">
-                            <button className='btn btn-primary'>Itika</button>
-                            <button className='btn btn-red'>Kataa</button>
-                        </div>
-                    </div>
-                ))
-            }
+          <div className="action">
+            <button className="btn btn-primary">Accepter</button>
+            <button className="btn btn-red">Refuser</button>
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
